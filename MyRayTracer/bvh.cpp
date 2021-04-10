fd052134823142878ca3025701c7460c2ecb84bb
Florian Lackner <lacknerflo@gmail.com>
2021-04-10 13:58:53 +0100

    WIP: implement BVH builder

--- MyRayTracer/bvh.cpp
+++ MyRayTracer/bvh.cpp
@@ -1,5 +1,6 @@
 #include "rayAccelerator.h"
 #include "macros.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -25,36 +26,69 @@ BVH::BVH(void) {}
 int BVH::getNumObjects() { return objects.size(); }
 
 
-void BVH::Build(vector<Object *> &objs) {
+void BVH::Build(vector<Object *> &objs) {		
+	BVHNode *root = new BVHNode();
 
-		
-			BVHNode *root = new BVHNode();
+	Vector min = Vector(FLT_MAX, FLT_MAX, FLT_MAX), max = Vector(-FLT_MAX, -FLT_MAX, -FLT_MAX);
+	AABB world_bbox = AABB(min, max);
 
-			Vector min = Vector(FLT_MAX, FLT_MAX, FLT_MAX), max = Vector(-FLT_MAX, -FLT_MAX, -FLT_MAX);
-			AABB world_bbox = AABB(min, max);
-
-			for (Object* obj : objs) {
-				AABB bbox = obj->GetBoundingBox();
-				world_bbox.extend(bbox);
-				objects.push_back(obj);
-			}
-			world_bbox.min.x -= EPSILON; world_bbox.min.y -= EPSILON; world_bbox.min.z -= EPSILON;
-			world_bbox.max.x += EPSILON; world_bbox.max.y += EPSILON; world_bbox.max.z += EPSILON;
-			root->setAABB(world_bbox);
-			nodes.push_back(root);
-			build_recursive(0, objects.size(), root); // -> root node takes all the 
-		}
+	for (Object* obj : objs) {
+		AABB bbox = obj->GetBoundingBox();
+		world_bbox.extend(bbox);
+		objects.push_back(obj);
+	}
+	world_bbox.min.x -= EPSILON; world_bbox.min.y -= EPSILON; world_bbox.min.z -= EPSILON;
+	world_bbox.max.x += EPSILON; world_bbox.max.y += EPSILON; world_bbox.max.z += EPSILON;
+	root->setAABB(world_bbox);
+	nodes.push_back(root);
+	build_recursive(0, objects.size(), root); // -> root node takes all the 
+	}
 
 void BVH::build_recursive(int left_index, int right_index, BVHNode *node) {
-	   //PUT YOUR CODE HERE
+	int num_objs = right_index - left_index;
 
+	// TODO: recursion stop test
 
-		//right_index, left_index and split_index refer to the indices in the objects vector
-	   // do not confuse with left_nodde_index and right_node_index which refer to indices in the nodes vector. 
-	    // node.index can have a index of objects vector or a index of nodes vector
-			
-		
+	// check which axis has the largest range of centroids
+	float mins[3] = {  FLT_MAX };
+	float maxs[3] = { -FLT_MAX };
+	for (int i = 0; i < num_objs; i++) {
+		Object* obj = objects[i+left_index];
+		AABB bbox = obj->GetBoundingBox();
+		float centroid[3] = { 
+			bbox.centroid().x,
+			bbox.centroid().y,
+			bbox.centroid().z,
+		};
+
+		for (int d = 0; d < 3; d++) {
+			if (centroid[d] < mins[d]) {
+				mins[d] = centroid[d];
+			}
+
+			if (centroid[d] > maxs[d]) {
+				maxs[d] = centroid[d];
+			}
+		}
 	}
+	float diffs[3];
+	int max_dim = 0;
+	for (int d = 0; d < 3; d++) {
+		diffs[d] = maxs[d] - mins[d];
+		if (diffs[d] > diffs[max_dim]) {
+			max_dim = d;
+		}
+	}
+
+	// sort objects along this axis
+	Comparator cmp;
+	cmp.dimension = max_dim;
+	std::sort(objects.begin() + left_index, objects.begin() + right_index, cmp);
+
+	//right_index, left_index and split_index refer to the indices in the objects vector
+	// do not confuse with left_nodde_index and right_node_index which refer to indices in the nodes vector. 
+	// node.index can have a index of objects vector or a index of nodes vector		
+}
 
 bool BVH::Traverse(Ray& ray, Object** hit_obj, Vector& hit_point) {
 			float tmp;
